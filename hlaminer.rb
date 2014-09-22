5a78b2e5721616666ea0d529b82feb1f0b4276e8
warrenlr <rwarren@bcgsc.ca>
2014-09-22 12:47:42 -0700

    Update hlaminer.rb

--- hlaminer.rb
+++ hlaminer.rb
@@ -6,7 +6,7 @@ class HLAminer < Formula
   #tag "genomemedicine"
   url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.1/HLAminer.tar.gz"
   version "1.1.0"
-  sha1 "5df39ea10a66ad5df1456b82e6ad3c050c10560e"
+  sha1 ""
 
   def install
     bin.install "HLAminer.pl","formatdb","blastall","HPRArnaseq_classI-II.sh","HPRArnaseq_classI.sh","HPRAwgs_classI-II.sh","HPRAwgs_classI.sh","HPTASRrnaseq_classI-II.sh","HPTASRrnaseq_classI.sh","HPTASRwgs_classI-II.sh","HPTASRwgs_classI.sh","ncbiBlastConfig.tx","parseXMLblast.p","TASR
