f2848576622f67be65ebfae5b8b7319f106a0e5a
warrenlr <rwarren@bcgsc.ca>
2014-09-22 13:49:27 -0700

    Update TASR.rb

--- TASR.rb
+++ TASR.rb
@@ -1,12 +1,12 @@
 require "formula"
 
-class TASR < Formula
+class tasr < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/tasr"
   #doi "10.1371/journal.pone.0019816"
-  #tag "PLOSone"
+  #tag "Bioinformatics"
   url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1.5.1-tar.gz"
   version "1.5.1"
-  sha1 ""
+  sha1 "89e3661d139c2eff760c0bba98e578b9"
 
   def install
     bin.install "TASR"
