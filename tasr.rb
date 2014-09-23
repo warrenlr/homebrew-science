aabe9ad61b8eaf94169748735f458592b15cfa17
warrenlr <rwarren@bcgsc.ca>
2014-09-23 16:09:48 -0700

    Update tasr.rb

--- tasr.rb
+++ tasr.rb
@@ -3,18 +3,18 @@ require "formula"
 class tasr < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/tasr"
   #doi "10.1371/journal.pone.0019816"
-  #tag "Bioinformatics"
+  #tag "bioinformatics"
   url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1.5.1-tar.gz"
   version "1.5.1"
   sha1 "65bbd736d4e3b2acc10c5e8926c419c34a523217"
 
   def install
-    bin.install "TASR"
-    doc.install "TASR.readme"
+    bin.install "tasr"
+    doc.install "tasr.readme"
     prefix.install "test", "tools"
   end
 
   test do
-    system "TASR |grep TASR"
+    system "tasr |grep tasr"
   end
 end
