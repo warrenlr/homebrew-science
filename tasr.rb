457bb5a176b2b41280631d8ac103e6e4dc605e17
Rene Warren <rwarren@bcgsc.ca>
2014-09-26 14:38:40 -0400

    Update tasr.rb
    fixed class names and point to new downloadable filename

--- tasr.rb
+++ tasr.rb
@@ -1,20 +1,20 @@
 require "formula"
 
-class tasr < Formula
+class Tasr < Formula
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/tasr"
   #doi "10.1371/journal.pone.0019816"
   #tag "bioinformatics"
-  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1.5.1-tar.gz"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1-5-1.tar.gz"
   version "1.5.1"
   sha1 "65bbd736d4e3b2acc10c5e8926c419c34a523217"
 
   def install
-    bin.install "tasr"
-    doc.install "tasr.readme"
+    bin.install "TASR"
+    doc.install "TASR.readme"
     prefix.install "test", "tools"
   end
 
   test do
-    system "tasr |grep tasr"
+    system "TASR |grep TASR"
   end
 end
