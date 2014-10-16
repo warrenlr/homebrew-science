0d7ded70801dc2455603e4a96ed456c6827de268
Rene Warren <rwarren@bcgsc.ca>
2014-10-16 16:34:04 -0700

    Update ssake.rb
    updated sha1

--- ssake.rb
+++ ssake.rb
@@ -6,7 +6,7 @@ class Ssake < Formula
   #tag "bioinformatics"
   url "http://www.bcgsc.ca/platform/bioinfo/software/ssake/releases/3.8.2/ssake_v3-8-2.tar.gz"
   version "3.8.2"
-  sha1 "aa8c6411e2df3256d5bf84bede6200f24b6d7f77"
+  sha1 "b9e2090198ec1b51eeb984088980f07eba1e1f4a"
 
   def install
     bin.install "SSAKE"
