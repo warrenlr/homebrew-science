8e81d310ad76e402978875bd9bca611ad41509ad
Rene Warren <rwarren@bcgsc.ca>
2015-05-20 14:01:04 -0700

    Update links-scaffolder.rb
    updated sha256sum

--- links-scaffolder.rb
+++ links-scaffolder.rb
@@ -5,7 +5,7 @@ class LinksScaffolder < Formula
 
   url "http://www.bcgsc.ca/platform/bioinfo/software/links/releases/1.5/links_v1-5.tar.gz"
   version "1.5"
-  sha256 "20b42024fa9da153512167c1ec4ebe75bfbf1118642e1ac964f40cabc400df04"
+  sha256 "b06f7d4c4c9de4e6c723d54fd2a2786f236a295166d3d36b9a166ecab36c5161"
 
   depends_on "Bloom::Faster" => :perl
 
