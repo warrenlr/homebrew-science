dfadc70534ceae9c65cde805581718d57030c18a
Rene Warren <rwarren@bcgsc.ca>
2016-03-02 08:38:47 -0800

    Update tasr.rb
    update to v1.6

--- tasr.rb
+++ tasr.rb
@@ -3,12 +3,13 @@ class Tasr < Formula
   # doi "10.1371/journal.pone.0019816"
   # tag "bioinformatics"
 
-  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.5.1/tasr_v1-5-1.tar.gz"
-  version "1.5.1"
-  sha256 "2101283f6a58b1ce83d29f09ac55d52a6ac863170ebafb3b4844be33a87cbcc1"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/tasr/releases/1.6/tasr_v1-6.tar.gz"
+  version "1.6.0"
+  sha256 "efb39416d28edabba1d5af5dd9f68fa9e41619e9d5f5615ca6d92b99a2997f1d"
 
   bottle do
     cellar :any
+    ###These need to be updated
     sha256 "3e5f480a3d5b9121acb526f8b444cd4b52f47698763c02fe65e07de1df70d1b5" => :yosemite
     sha256 "c3d1d87211da6d353c3aa88f5370baf6104474eecff571fc011d7c228d222349" => :mavericks
     sha256 "7707c5fcdf98f4ebf64e1f828dc824bf5b839571bf7b76c248e0d8275d001e1c" => :mountain_lion
