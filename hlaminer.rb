650b37d985c7d852e743f34b9eb0cb89745dcdcd
Rene Warren <rwarren@bcgsc.ca>
2016-03-02 08:36:42 -0800

    Update hlaminer.rb
    update to v1.3

--- hlaminer.rb
+++ hlaminer.rb
@@ -3,12 +3,13 @@ class Hlaminer < Formula
   # doi "10.1186/gm396"
   # tag "bioinformatics"
 
-  url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.1/HLAminer.tar.gz"
-  version "1.1.0"
-  sha256 "2c3e4458215f06764f58e8f3d95fb8e3a47d2a19152478d047453944c8cb84c3"
+  url "http://www.bcgsc.ca/platform/bioinfo/software/hlaminer/releases/1.3/HLAminer_v1-3.tar.gz"
+  version "1.3.0"
+  sha256 "3c30ff02df31a839968c6694b3bed404c262a51fe2830711642e2210f00e68bb"
 
   bottle do
     cellar :any
+    ###These need to be updated
     sha256 "100293fce1617b7ea2dd05e7ac4c0f215765a3be80d109e87168e67ceae7a527" => :yosemite
     sha256 "6eb37791b21c0c5cc1c6f446cb82561ca8f9b876be72f3bc296ca77de6ad8230" => :mavericks
     sha256 "30f704aa898b243cc0fea2ef1c70f8602a88c41422548881703f3647f336a21d" => :mountain_lion
